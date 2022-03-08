3c1cd56c58a464f7be3e731e9f6a77d9b2036f40
RenatoCPereira <renatocarlos8960@gmail.com>
2022-03-07 22:34:30 -0300

    Adicionei um metodo

--- Capgemini/src/br/com/questao2/VetorManual.java
+++ Capgemini/src/br/com/questao2/VetorManual.java
@@ -1,6 +1,8 @@
 package br.com.questao2;
 
-public class Vetor {
+import java.util.Scanner;
+
+public class VetorManual {
 
 	// METODO UTILIZADO PARA FAZER A COMPARAÇÃO DE CADA POSIÇÃO DO ARRAY
 	public static int Metodo(int[] array, int x) {
@@ -32,12 +34,30 @@ public class Vetor {
 
 	public static void main(String[] args) {
 
-		// ARRAY DO EXERCIO UTILIZADO PARA O DESENVOLVIMENTO
-		int[] array = { 1, 5, 3, 4, 2 };
+		// CASO O USUARIO QUEIRA DIGITAR OS DADO MANUALMENTE
 
-		int resultado = Metodo(array, 2);
+		Scanner teclado = new Scanner(System.in);
+
+		int i;
+		int x;
+		int N;
+
+		System.out.println("Quantos números você vai querer digitar? ");
+		N = teclado.nextInt();
+
+		int[] array = new int[N];
+
+		for (i = 0; i < N; i++) {
+			System.out.println("Digite um número: ");
+			array[i] = teclado.nextInt();
+		}
+		System.out.println("Digite um valor para x: ");
+		x = teclado.nextInt();
+
+		int resultado = Metodo(array, x);
 
 		System.out.println("Existem  " + resultado + "  pares ");
 
 	}
+
 }
